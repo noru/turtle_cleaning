b06b9ae30758e4be25d7b15625ca9a4f9b514976
xiuxiuxiu <nooooru@gmail.com>
2018-11-01 16:47:28 +0800

    update

--- src/turtle_cleaning.py
+++ src/turtle_cleaning.py
@@ -13,44 +13,50 @@ z = 0
 yaw = 0
 
 def poseCallback(pose):
-	global x, y, z, yaw
-	x = pose.x
-	y = pose.y
-	yaw = pose.theta
+    global x, y, z, yaw
+    x = pose.x
+    y = pose.y
+    yaw = pose.theta
 
 def move(speed, distance, isForward):
 
-	twist = Twist()
-	global x, y
-	x0 = x
-	y0 = y
-	if isForward:
-		twist.linear.x = abs(speed)
-	else:
-		twist.linear.x = -abs(speed)
+    twist = Twist()
+    global x, y
+    x0 = x
+    y0 = y
+    if isForward:
+        twist.linear.x = abs(speed)
+    else:
+        twist.linear.x = -abs(speed)
 
-	distance_moved = 0.0
-	loop = rospy.Rate(10)
-	cmd_vel_topic = '/turtle1/cmd_vel'
-	pub = rospy.Publisher(cmd_vel_topic, Twist, queue_size = 10)
+    distance_moved = 0.0
+    loop = rospy.Rate(10)
+    cmd_vel_topic = '/turtle1/cmd_vel'
+    pub = rospy.Publisher(cmd_vel_topic, Twist, queue_size = 10)
 
-	while True:
-		pub.publish(twist)
-		loop.sleep()	
-		distance_moved += abs(0.5 * math.sqrt((x - x0) ** 2 + (y - y0) ** 2)) # // todo
+    while True:
+        pub.publish(twist)
+        loop.sleep()
+        distance_moved += abs(0.5 * math.sqrt((x - x0) ** 2 + (y - y0) ** 2)) # // todo
+        if (distance_moved > distance):
+            break
+
+    twist.linear.x = 0
+    pub.publish(twist)
+
 
 
 if __name__ == '__main__':
 
     try:
-		rospy.init_node('turtle_cleaning', anonymous = True)
-		
-		cmd_vel_topic = '/turtle1/cmd_vel'
-		pub = rospy.Publisher(cmd_vel_topic, Twist, queue_size = 10)
-		
-		pose_topic = '/turtle1/pose'
-		sub = rospy.Subscriber(pose_topic, Pose, poseCallback)
+        rospy.init_node('turtle_cleaning', anonymous = True)
 
-		time.sleep(2)
+        cmd_vel_topic = '/turtle1/cmd_vel'
+        pub = rospy.Publisher(cmd_vel_topic, Twist, queue_size = 10)
+
+        pose_topic = '/turtle1/pose'
+        sub = rospy.Subscriber(pose_topic, Pose, poseCallback)
+
+        time.sleep(2)
     except:
         print 'exception'
